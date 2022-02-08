4795dd1b6896ffbb894cbaf0f03b63add8173d49
mhdraslan <mhdraslan@outlook.com>
2022-02-08 14:14:07 +0400

    Update Deploy-Etisalat-Az-Policies.ps1

--- Deploy-Etisalat-Az-Policies.ps1
+++ Deploy-Etisalat-Az-Policies.ps1
@@ -1,11 +1,13 @@
-$TopLevelManagemntGroupId = "Etisalat"
+$TopLevelManagemntGroupId = "ESLZ"
 New-AzManagementGroupDeployment -Name "EtisalatAzPoliciesImport" `
                                 -ManagementGroupId $TopLevelManagemntGroupId `
                                 -Location "UAENorth" `
                                 -TemplateFile C:\Repos\EESLZPolicies\policies.json
 
+<#
 New-AzManagementGroupDeployment -Name "Etisalat-AzInitiatives-Import" `
                                 -ManagementGroupId $TopLevelManagemntGroupId `
                                 -Location UAENorth `
                                 -TemplateFile C:\Repos\EESLZPolicies\Initiatives.json `
                                 -mgmtGroupId $TopLevelManagemntGroupId
+#>
